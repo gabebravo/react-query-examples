d4f701c0d83f4f13720b367864d7e163d8d2cc53
Gabe Bravo <gabecbravo@gmail.com>
2021-04-24 20:24:45 -0400

    added some comments to useGetPostsByUserId to show an example of how initialData would work

--- src/hooks/useGetPostsByUserId.js
+++ src/hooks/useGetPostsByUserId.js
@@ -12,4 +12,19 @@ const useGetPostsByUserId = (
     ...config,
   })
 
+// NOTE : initialData config object : the user would see this post list
+// the query would still fetch, and then the posts would get updated with the actual data
+/*
+  useQuery(['userInfo', id], () => getPostsByUserId(id), {
+    { initialData: [
+      { id: 1, title: 'The art of cloud watching' },
+      { id: 2, title: 'Dog voices, are they real?' },
+      { id: 3, title: 'Global Warming... take it serious' },
+      { id: 4, title: 'Tacos or Pizza, food review' },
+      { id: 5, title: '10 Best Movies you\'ve never seen' },
+    ], initialStale: true },
+  })
+*/
+// initialStale: true >> IS IMP to get this to work
+
 export default useGetPostsByUserId
