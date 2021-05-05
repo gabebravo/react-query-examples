b44ef00153ab4ecab1d71d8da797500fa7daa75e
Gabe Bravo <gabecbravo@gmail.com>
2021-05-04 23:12:10 -0400

    Added a rendering test

--- src/views/PokemonList/PokemonList.test.js
+++ src/views/PokemonList/PokemonList.test.js
@@ -1,6 +1,8 @@
+import { render, screen, prettyDOM } from '@testing-library/react'
 import { renderHook } from '@testing-library/react-hooks'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import usePokemonList from '../../hooks/usePokemonList'
+import PokemonList from './PokemonList'
 
 const wrapper = ({ children }) => {
   const queryClient = new QueryClient()
@@ -11,12 +13,32 @@ const wrapper = ({ children }) => {
   )
 }
 
-test('Render PokemonList RQ', async () => {
+test('usePokemonList returns data', async () => {
   const { result, waitFor } = renderHook(() => usePokemonList(), {
     wrapper,
   })
 
   await waitFor(() => result.current.isSuccess)
-  console.log(`result.current.data`, result.current.data)
   expect(result.current.data).toHaveLength(20)
 })
+
+test('while loading', () => {
+  jest.mock('../../hooks/usePokemonList', () => {
+    return {
+      __esModule: true,
+      default: jest.fn(() => {}),
+    }
+  })
+
+  render(<PokemonList />, { wrapper })
+  const header = screen.getByRole('heading')
+  expect(header).toHaveTextContent(/loading.../i)
+})
+
+// test('with an error', () => {
+//   // it.todo('renders an error message')
+// })
+
+// test('with data', () => {
+//   // it.todo('renders the list of pokemon')
+// })
